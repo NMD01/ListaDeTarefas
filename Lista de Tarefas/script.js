aef76c626152625b5b1ff2b6800d41fd2cdd1da9
Nathan Martini Duarte <77181059+NMD01@users.noreply.github.com>
2021-04-18 15:02:21 -0300

    Add files via upload

--- Lista de Tarefas/script.js
+++ Lista de Tarefas/script.js
@@ -1,10 +1,19 @@
 
-let tarefas = []
+let tarefas = []   //array vazio
 
 
 if (localStorage.hasOwnProperty("tarefas")) {
     tarefas = JSON.parse(localStorage.getItem("tarefas"))
-  }
+  } //verifica se já tem um array com a kay "tarefas" no localstorage.Se tiver, já carrega o array "tarefas" vazio com os dados que estavam armazenados no localstorage
+
+
+if(tarefas == 0){
+    let limpar = document.getElementById("limpar")
+    let h2 = document.getElementById("h2")
+    
+    limpar.style.display = "none"
+    h2.style.display = "none"
+}//esconde o botão limpar tudo
 
   
 console.log(tarefas)
@@ -22,48 +31,82 @@ novaTarefa = function(){
     
         location.reload()     
   }
-}
+} // função para adicionar items dentro do array "tarefas" e salvar as alterações no local storage
+
 
 let adicionar = document.getElementById("adicionar")
 adicionar.addEventListener("click", novaTarefa)
 
+
+
+
+
+
+
+
+
 for(i=0; i < tarefas.length; i++){
     let res = document.getElementById("res")
+
+
 
     let resposta = document.createElement("div")
     resposta.innerHTML += `<strong>${tarefas[i].nome}:</strong> ${tarefas[i].fazer}`
     resposta.setAttribute("class","res")
     resposta.setAttribute("id",length++)
     res.appendChild(resposta) 
+    //vai criar uma div para mostrar os items dentro do array "tarefas"
+    
+    
 
+
+    let excluir = document.createElement("input")
+    excluir.setAttribute("type","Button")
+    excluir.setAttribute("class", "botaoPadrao excluir")
+    excluir.value = "Excluir"
+    resposta.appendChild(excluir)
+    excluir.addEventListener("click",apagar = function(){
+        res.removeChild(resposta)
+        localStorage.setItem("resposta",JSON.stringify(res))
+        //location.reload()
+    
+    /* Eu tentei de todos os jeitos retirar um elemento especifico da lista (tarefas que está no localstorage) mas não consegui, ele sempre tirava o ultimo item adicionado.
+    Então usando o removeChild eu consegui remover a div q aparecia os item da lista, mas não consigo salvar essas alterações no local storage, se recarregar o item da lista retorna
+      */
+    })
+    
+    
+    
     let concluir = document.createElement("input")
     concluir.setAttribute("type","button")
     concluir.setAttribute("value","concluir")
-
     resposta.appendChild(concluir)
-    concluir.setAttribute("class","botaoPadrao")
+    concluir.setAttribute("class","botaoPadrao concluir")
     concluir.addEventListener("click", terminar=function(){
 
         if(concluir.value === "concluir"){
             concluir.value = "Refazer"
             resposta.setAttribute("class", "botaoConcluir")
+            concluir.setAttribute("class","refazer botaoPadrao")
         }else{
             resposta.setAttribute("class", "res")
             concluir.value = "concluir"
+            concluir.removeAttribute("class","refazer")
+            concluir.setAttribute("class","botaoPadrao concluir")
+         //O código acima esta funcionando bem, mas eu não sei como salvar as alterações do css no localStorage, se recarregar a pagina some tudo
+
         }
 
-        //O código acima esta funcionando bem mas eu não sei como salvar as alterações do css no localStorage, se recarregar a pagina some tudo
 
 
+        
 
+        
 
-        /* tarefas.pop()
-        localStorage.setItem("tarefas",JSON.stringify(tarefas))
-        location.reload() */
-
-        //Eu tentei de todos os jeitos, retirar um elemento especifico da lista mas não consegui, ele sempre tira o ultimo item adicionado 
+        
 
     })
+    
 }
 
 limpar = function(){
